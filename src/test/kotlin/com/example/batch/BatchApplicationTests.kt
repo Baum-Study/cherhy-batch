943c218082844b0cacf7e3d209d238ea53c2357d
lolmageap <ekxk1234@naver.com>
2024-11-08 20:36:54 +0900

    chapter1 실습

--- src/test/kotlin/com/example/batch/BatchApplicationTests.kt
+++ src/test/kotlin/com/example/batch/BatchApplicationTests.kt
@@ -1,13 +1,10 @@
 package com.example.batch
 
+import io.kotest.core.spec.style.StringSpec
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BatchApplicationTests {
+class BatchApplicationTests: StringSpec({
 
-    @Test
-    fun contextLoads() {
-    }
-
-}
+})
