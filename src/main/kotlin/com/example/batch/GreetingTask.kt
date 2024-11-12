9490e437137308445452782d206d9aa96c45aad0
lolmageap <ekxk1234@naver.com>
2024-11-12 23:34:57 +0900

    create greeting task

---
+++ src/main/kotlin/com/example/batch/GreetingTask.kt
@@ -0,0 +1,25 @@
+package com.example.batch
+
+import mu.KotlinLogging
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.InitializingBean
+
+class GreetingTask : Tasklet, InitializingBean {
+    private val logger = KotlinLogging.logger {}
+
+    override fun execute(
+        contribution: StepContribution,
+        chunkContext: ChunkContext,
+    ): RepeatStatus {
+        logger.info { "------------------ Task Execute -----------------" }
+        logger.info { "GreetingTask : $contribution, $chunkContext" }
+        return RepeatStatus.FINISHED
+    }
+
+    override fun afterPropertiesSet() {
+        logger.info { "----------------- After Properties Set --------------" }
+    }
+}
