87db6d625b7164386ebad3908f326a208851a88e
lolmageap <ekxk1234@naver.com>
2024-12-11 22:00:42 +0900

    create settlement batch job test

--- src/test/kotlin/com/example/batch/CreateFileTests.kt
+++ src/test/kotlin/com/example/batch/CreateFileTests.kt
@@ -1,18 +1,39 @@
 package com.example.batch
 
+import com.example.batch.SettlementConfiguration.Companion.SETTLEMENT_JOB
+import com.example.batch.lib.JobParameterFactory
 import com.example.batch.lib.PaymentFactory
 import com.example.batch.lib.mapParallel
 import io.kotest.core.spec.style.StringSpec
+import org.springframework.batch.core.Job
+import org.springframework.batch.test.JobLauncherTestUtils
+import org.springframework.batch.test.JobRepositoryTestUtils
+import org.springframework.batch.test.context.SpringBatchTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ClassPathResource
 import java.io.File
 import java.io.PrintWriter
 
 // 참고 했던 블로그 : https://jojoldu.tistory.com/525
-class CreateFileTests : StringSpec({
+@SpringBootTest
+@SpringBatchTest
+class CreateFileTests(
+    @Autowired private val jobLauncherTestUtils: JobLauncherTestUtils,
+    @Autowired private val jobRepositoryTestUtils: JobRepositoryTestUtils,
+    @Autowired @Qualifier(SETTLEMENT_JOB) private val settlementJob: Job,
+) : StringSpec({
+    afterEach {
+        jobRepositoryTestUtils.removeJobExecutions()
+    }
+
     "랜덤 데이터를 생성해서 csv 파일을 만든다." {
         val payments = 10_000.mapParallel(PaymentFactory::generateRandom)
         val reader = LinkedListItemReader(payments)
 
-        val file = File("output/test-payments.csv")
+        val resource = ClassPathResource("output/test-payments.csv")
+        val file = File(resource.uri)
         file.parentFile.mkdirs()
 
         PrintWriter(file).use { writer ->
@@ -25,5 +46,12 @@ class CreateFileTests : StringSpec({
                 payment = reader.read()
             }
         }
+    }
+
+    "csv 파일로 만든 결제 데이터를 읽어서 정산한다." {
+        jobLauncherTestUtils.job = settlementJob
+        val job = JobParameterFactory.create(SETTLEMENT_JOB)
+
+        jobLauncherTestUtils.launchJob(job)
     }
 })
