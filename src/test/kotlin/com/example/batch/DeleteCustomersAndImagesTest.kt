62fc050db85ec42471f0c68a95ef21155a98c0a3
lolmageap <ekxk1234@naver.com>
2024-12-19 22:15:18 +0900

    success test

--- src/test/kotlin/com/example/batch/DeleteCustomersAndImagesTest.kt
+++ src/test/kotlin/com/example/batch/DeleteCustomersAndImagesTest.kt
@@ -3,7 +3,6 @@ package com.example.batch
 import com.example.batch.config.CustomerRepository
 import com.example.batch.config.DeleteCustomerConfiguration.Companion.DELETE_CUSTOMER_JOB
 import com.example.batch.config.ImageRepository
-import com.example.batch.config.SettlementConfiguration.Companion.SETTLEMENT_JOB
 import com.example.batch.lib.CustomerFactory
 import com.example.batch.lib.ImageFactory
 import com.example.batch.lib.JobParameterFactory
@@ -11,6 +10,7 @@ import com.example.batch.lib.mapParallel
 import com.example.batch.model.Status
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import jakarta.persistence.EntityManagerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.test.JobLauncherTestUtils
 import org.springframework.batch.test.JobRepositoryTestUtils
@@ -18,6 +18,12 @@ import org.springframework.batch.test.context.SpringBatchTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.transaction.support.TransactionTemplate
+import kotlin.time.measureTime
 
 // 참고 했던 블로그 : https://jojoldu.tistory.com/551
 @SpringBootTest
@@ -25,11 +31,12 @@ import org.springframework.boot.test.context.SpringBootTest
 class DeleteCustomersAndImagesTest(
     @Autowired private val jobLauncherTestUtils: JobLauncherTestUtils,
     @Autowired private val jobRepositoryTestUtils: JobRepositoryTestUtils,
-    @Autowired @Qualifier(DELETE_CUSTOMER_JOB) private val settlementJob: Job,
+    @Autowired @Qualifier(DELETE_CUSTOMER_JOB) private val deleteCustomerJob: Job,
     @Autowired private val customerRepository: CustomerRepository,
     @Autowired private val imageRepository: ImageRepository,
+    @Autowired private val transactionTemplate: TransactionTemplate,
 ) : BehaviorSpec({
-    afterEach {
+    afterTest {
         jobRepositoryTestUtils.removeJobExecutions()
     }
 
@@ -37,39 +44,58 @@ class DeleteCustomersAndImagesTest(
         val customerCount = 100_000
         val imageCount = 1_000_000
 
-        val randomCustomers =
-            customerCount.mapParallel(CustomerFactory::generateRandom)
+        measureTime {
+            val randomCustomers = customerCount.mapParallel(CustomerFactory::generateRandom)
+            val randomImages =
+                imageCount.mapParallel {
+                    ImageFactory.generateRandom(randomCustomers.random())
+                }
 
-        val randomImages =
-            imageCount.mapParallel {
-                ImageFactory.generateRandom(randomCustomers.random())
+            transactionTemplate.executeWithoutResult {
+                customerRepository.saveAll(randomCustomers)
+                imageRepository.saveAll(randomImages)
+            }
+        }.let {
+            println("데이터 생성 소요 시간: $it")
+        }
+
+        val activeCustomerSize = customerRepository.countByStatus(Status.ACTIVE)
+        val activeImageSize =
+            transactionTemplate.writeTransaction {
+                val activeCustomers = customerRepository.findByStatus(Status.ACTIVE)
+                imageRepository.countByCustomerIn(activeCustomers)
             }
 
-        customerRepository.saveAll(randomCustomers)
-        imageRepository.saveAll(randomImages)
-
-        val allCustomerSize = customerRepository.count()
-        val allImageSize = imageRepository.count()
-
-        val deleteCustomers = customerRepository.findByStatus(Status.ACTIVE)
-        val deleteCustomerSize = deleteCustomers.size
-
-        val deleteImages = imageRepository.findByCustomerIn(deleteCustomers)
-        val deleteImageSize = deleteImages.size
-
         When("비활성화된 회원을 삭제하고 연결된 이미지도 삭제한다.") {
-            jobLauncherTestUtils.job = settlementJob
-            val job = JobParameterFactory.create(SETTLEMENT_JOB)
+            measureTime {
+                jobLauncherTestUtils.job = deleteCustomerJob
+                val job = JobParameterFactory.create(DELETE_CUSTOMER_JOB)
 
-            jobLauncherTestUtils.launchJob(job)
-
+                jobLauncherTestUtils.launchJob(job)
+            }.let {
+                println("배치 작업 총 소요 시간: $it")
+            }
             Then("삭제된 회원과 이미지의 수만큼 데이터베이스의 회원과 이미지 수가 감소한다.") {
-                val activeCustomerSize = customerRepository.countByStatus(Status.ACTIVE)
-                activeCustomerSize shouldBe allCustomerSize - deleteCustomerSize
-
-                val activeImageSize = imageRepository.count()
-                activeImageSize shouldBe allImageSize - deleteImageSize
+                customerRepository.count() shouldBe activeCustomerSize
+                imageRepository.count() shouldBe activeImageSize
             }
         }
     }
-})
+}) {
+    @TestConfiguration
+    class TestConfig {
+        @Bean(name = ["testTransactionManager"])
+        @Primary
+        fun testTransactionManager(
+            entityManagerFactory: EntityManagerFactory,
+        ) =
+            JpaTransactionManager().apply {
+                this.entityManagerFactory = entityManagerFactory
+            }
+    }
+}
+
+private fun <T> TransactionTemplate.writeTransaction(
+    block: () -> T,
+): T =
+    execute { block() }!!
