4e30a83aae44790df0f561977de8f1f984ac057a
lolmageap <ekxk1234@naver.com>
2025-01-05 22:42:30 +0900

    fix item writer to composite item writer

--- src/main/kotlin/com/example/batch/config/CompleteProductJobConfig.kt
+++ src/main/kotlin/com/example/batch/config/CompleteProductJobConfig.kt
@@ -2,6 +2,7 @@ package com.example.batch.config
 
 import com.example.batch.model.PurchaseStatus
 import com.example.batch.model.PurchasedProduct
+import com.example.batch.util.extension.sumOf
 import org.apache.ibatis.session.SqlSessionFactory
 import org.mybatis.spring.batch.builder.MyBatisBatchItemWriterBuilder
 import org.mybatis.spring.batch.builder.MyBatisPagingItemReaderBuilder
@@ -9,6 +10,9 @@ import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.item.Chunk
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.support.CompositeItemWriter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -36,7 +40,7 @@ class CompleteProductJobConfig(
         StepBuilder(COMPLETE_PRODUCT_STEP, jobRepository)
             .chunk<PurchasedProduct, PurchasedProduct>(1000, transactionManager)
             .reader(completeProductItemReader())
-            .writer(completeProductItemWriter())
+            .writer(compositeItemWriter())
             .build()
 
     @Bean(COMPLETE_PRODUCT_ITEM_READER)
@@ -53,6 +57,17 @@ class CompleteProductJobConfig(
             )
             .build()!!
 
+    @Bean(COMPOSITE_ITEM_WRITER)
+    fun compositeItemWriter() =
+        CompositeItemWriter<PurchasedProduct>().apply {
+            setDelegates(
+                listOf(
+                    completeProductItemWriter(),
+                    addMileageWriter(),
+                )
+            )
+        }
+
     @Bean(COMPLETE_PRODUCT_ITEM_WRITER)
     fun completeProductItemWriter() =
         MyBatisBatchItemWriterBuilder<PurchasedProduct>()
@@ -60,9 +75,31 @@ class CompleteProductJobConfig(
             .statementId(PRODUCT_WRITE_QUERY)
             .build()!!
 
+    @Bean
+    fun addMileageWriter(): ItemWriter<PurchasedProduct> =
+        ItemWriter { chunk: Chunk<out PurchasedProduct> ->
+            val mileageMap =
+                chunk.groupBy { it.customerId }
+                    .sumOf { it.price * 0.01.toBigDecimal() }
+
+            sqlSessionFactory.openSession().use { session ->
+                mileageMap.forEach { (customerId, price) ->
+                    session.update(
+                        ADD_MILEAGE_QUERY,
+                        mapOf(
+                            "customerId" to customerId,
+                            "price" to price,
+                        )
+                    )
+                }
+            }
+        }
+
     companion object {
+        const val COMPOSITE_ITEM_WRITER = "compositeItemWriter"
         const val PRODUCT_READ_QUERY = "com.example.batch.config.PurchaseRepository.findAll"
         const val PRODUCT_WRITE_QUERY = "com.example.batch.config.PurchaseRepository.updateDeliveryComplete"
+        const val ADD_MILEAGE_QUERY = "com.example.batch.config.MileageRepository.update"
         const val COMPLETE_PRODUCT_JOB = "completeProductJob"
         const val COMPLETE_PRODUCT_STEP = "completeProductStep"
         const val COMPLETE_PRODUCT_ITEM_READER = "completeProductItemReader"
